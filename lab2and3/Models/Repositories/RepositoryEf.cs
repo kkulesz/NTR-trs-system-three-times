82331090a150c2ceec1612b6591447938d6b888e
kkulesza <k.kulesza@stud.elka.pw.edu.pl>
2021-12-11 20:37:51 +0100

    just return null

--- lab2and3/Models/Repositories/RepositoryEf.cs
+++ lab2and3/Models/Repositories/RepositoryEf.cs
@@ -9,50 +9,50 @@ namespace lab2and3.Models.Repositories
     public class RepositoryEf : IRepository
     {
         public User GetUser(string login)
-        { return new User(""); }
+        { return null; }
 
         public List<User> GetAllUsers()
-        { return new List<User>(); }
+        { return null; }
         public User CreateUser(string login)
-        { return new User(""); }
+        { return null; }
 
         public Project GetProject(string projectName)
-        { return new Project("", "", 0, true, null); }
+        { return null; }
 
         public List<Project> GetAllProjects()
-        { return new List<Project>(); }
+        { return null; }
 
         public List<Project> GetAllProjectsForOwner(string owner)
-        { return new List<Project>(); }
+        { return null; }
 
         public Project CreateProject(Project project)
-        { return new Project("", "", 0, true, null); }
+        { return null; }
 
         public Project UpdateProject(Project project)
-        { return new Project("", "", 0, true, null); }
+        { return null; }
 
         public Activity GetActivity(string code)
-        { return new Activity("", "", "", 0, null, DateTime.Now, null, "", true); }
+        { return null; }
 
         public Activity CreateActivity(Activity activity)
-        { return new Activity("", "", "", 0, null, DateTime.Now, null, "", true); }
+        { return null; }
 
         public Activity UpdateActivity(Activity activity)
-        { return new Activity("", "", "", 0, null, DateTime.Now, null, "", true); }
+        { return null; }
 
         public List<Activity> GetAllActivities()
-        { return new List<Activity>(); }
+        { return null; }
 
         public List<Activity> GetActivitiesForUserForMonth(string executor, int year, int month)
-        { return new List<Activity>(); }
+        { return null; }
 
         public void DeleteActivity(string code, string executor)
         { }
 
         public UsersMonth GetUsersMonth(string executor, int year, int month)
-        { return new UsersMonth(0, 0, null, false); }
+        { return null; }
 
         public UsersMonth AcceptMonthForUser(UsersMonth month)
-        { return new UsersMonth(0, 0, null, false); }
+        { return null; }
     }
 }
