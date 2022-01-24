1fbb35e89d23259db67cfdcd7c3dd982c927738e
kkulesz <kulesza.konrad@protonmail.com>
2022-01-24 20:29:57 +0100

    WSL2 gigachad

--- lab4/api/routes/users.js
+++ lab4/api/routes/users.js
@@ -12,7 +12,7 @@ router
     .route('/')
     .post((req, res) => {
         const result = repo.createUser(req.body)
-        result ? res.send(result) : res.send(409) //conflict
+        result ? res.send(result) : res.send(409) // conflict
     })
     .get((req, res) => {
         res.send(repo.getUsers())
